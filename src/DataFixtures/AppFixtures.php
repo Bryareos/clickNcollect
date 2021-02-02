41b3af6b1927d7a07181e4696278d00e7a1b0c90
Bryareos <david-o@jhiki.fr>
2021-02-02 15:19:47 +0100

    create fastfood category and big mac product

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use Cocur\Slugify\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
@@ -12,7 +15,20 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        $fastFood = new Category();
+        $name = 'Fast Food';
+        $fastFood->setName($name);
+        $slugify = new Slugify();
+        $fastFood->setSlug($slugify->slugify($name));
 
+        $manager->persist($fastFood);
+
+        $bigMac = new Product();
+        $bigMac->setName('Big Mac');
+        $bigMac->setDescription('Le big mac le seul l\'unique, le classique');
+        $bigMac->setPrice(4);
+        $bigMac->setQuantity(10);
+        $bigMac->setCategoryId($fastFood);
         $manager->flush();
     }
 }
