4df0734793187d78aee738f103b0e02c5af6ecbe
Franck <franckjung@yahoo.fr>
2021-02-03 12:07:47 +0100

    forget to use annotation/groups

--- src/Entity/Store.php
+++ src/Entity/Store.php
@@ -6,6 +6,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\StoreRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
